342b0a2efd2cd8120c2f8092f5fbe677786024e8
jacobmeyer1207 <jacobmeyer1207@gmail.com>
2026-07-07 21:31:35 -0600

    Update DMNC_main.py

--- DMNC_main.py
+++ DMNC_main.py
@@ -3,36 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as rand
 import time
+import pandas as pd
+from scipy.stats import ks_2samp
+from scipy.stats import wasserstein_distance
+
+import pip
+print(pip.__version__) 
+
+
 
 import DMNC_Detector as dmnc_det     # Comes with plt, np, rand, time
 
 
-# Less safe import statement, because DMNC_Rates is currently global
-#      namespace, and is not contained within a class.
-from DMNC_Rates import *    # Access to many fundamental calculations
+import DMNC_Rates as rts   # Access to many fundamental calculations
+rates = rts.Rates()
 
-'''Warning: This will bring in all the free parameters and functions
-            from DMNC_Rates without ANY namespace protection. In
-            particular, DO NOT overwrite the following variables:
-            
-            Z
-            A
-            e
-            mu
-            V0
-            k
-            R
-            levels
-            
-            Note that the functions defined in DMNC_Rates.py will
-            also exist in the global namespace. They have rather
-            specific names, but do double check you don't overwrite
-            anything while using these files.
-            
-            This can be made safer by eventually giving DMNC_Rates.py
-            a class inside of it, but this will take a rather long
-            amount of time, given how many "self." typings I'll need
-            to add to the file.'''
 
 # Functions**********************************************************
 
@@ -98,7 +83,7 @@ det_height = 14                        # meters
 # FIXME: R value from Rates is used, not this one. I think I'll have
 # to turn Rates into a class so I can modify values.
 R = 10                                 # DM radius, GeV^-1
-x_sec_dict = xsec_v_tot_S()            # keys: states, vals: X sects
+x_sec_dict = rates.xsec_v_tot_S()            # keys: states, vals: X sects
 xsec_tot = sum_dict_vals(x_sec_dict)   # Total cross section, GeV^-2
 
 # hc is 1240 eV * nm: e-9 for eV -> GeV, e-7 for nm -> cm
@@ -115,8 +100,6 @@ num_density_LAr = 1.39 * 6.02e23 / 39.948
 # I comment out the ones not in use, but save them because
 # they are still useful for seeing how things are working.
 
-det = dmnc_det.Detector(det_length, det_width, det_height, num_density_LAr, xsec_cm, x_sec_dict)
-
 '''
 for i in range(500000):
     face_count[det.random_face()] += 1
@@ -126,9 +109,9 @@ plt.grid(True)
 plt.xlim(0, 7)
 plt.bar(x=[i for i in range(1, 7)], height=[face_count[key] for key in face_count.keys()], tick_label = list(face_count.keys()))
 plt.show()
-'''
 
-'''
+
+
 for i in range(1000):
     det.random_entrance()
     if not det.particle_in_det():
@@ -141,20 +124,91 @@ for i in range(1000):
         break
     if i == 99:
         print('Success! All tests passed')
+
+
 '''
 
 
-for i in range(1):
+def Capture_stats():
+    det = dmnc_det.Detector(det_length, det_width, det_height, num_density_LAr, xsec_cm, x_sec_dict)
+    avg_phot_energy_list = []
+    decay_num_list = []
     det.random_entrance()
     det.gen_capture_locs()
-print(det.capture_locs)
+    
+    start_time = time.monotonic()
 
+    det.photon_generation()
+    end_time = time.monotonic()
+    duration = end_time - start_time
+    print('Time to generate all decays:', format_seconds(duration))
+    capture_num = len(det.capture_locs.items())
+    if capture_num > 0:
+        for i in det.phot_4_vec_list:
+            curr_avg_photon_energy = 0
+            curr_decay_num = len(i)
+            for j in i:
+                curr_avg_photon_energy += j[0]
+            curr_avg_photon_energy /= curr_decay_num
+            avg_phot_energy_list.append(curr_avg_photon_energy)
+            decay_num_list.append(curr_decay_num)
+        return(det.phot_4_vec_list, capture_num, decay_num_list, duration, avg_phot_energy_list, det.capture_locs)
+    else:
+        print("failed a capture")
+        return Capture_stats()
+
+
+def HepMC_data():
+    data = Capture_stats()
+    capture_locs = list(data[5].keys())
+    j = 1
+    for i in data[0]:
+        E = 0
+        px = 0
+        py = 0
+        pz = 0
+        print("Event:", j)
+        print("Units: GeV, Meters")
+        print("Vertex:", capture_locs[j-1]) #meters from the origin
+        l = 1
+        for k in i:
+            print("photon ", l, ", 4-vector:", k)
+            E += k[0]
+            px += k[1]
+            py += k[2]
+            pz += k[3]
+            l += 1
+        avg_vec = [E/len(i), px/len(i), py/len(i), pz/len(i)]
+        print("average photon 4-vector:", avg_vec)
+        print("Capture of Argon atom produced", data[2][j-1],"photons.")
+        j+=1
+
+HepMC_data()
+####Testing methods of sampling angles######################
 '''
-start_time = time.monotonic()
-
-det.photon_generation()
-
-end_time = time.monotonic()
-duration = end_time - start_time
-print('Time to generate all decays:', format_seconds(duration))
+det = dmnc_det.Detector(det_length, det_width, det_height, num_density_LAr, xsec_cm, x_sec_dict)
+det.gen_capture_locs()
+Rejection_samples = []
+Inversion_samples = []
+key = list(det.capture_locs.keys())[0]
+state = det.capture_locs[key]
+ni = state[0]
+li = state[1]
+mi = state[2]
+decay_dict = rates.Gamma_tot_B(ni, li, mi)
+new_state = key_val_by_weight(decay_dict)[0]
+nf = new_state[0]
+lf = new_state[1]
+mf = new_state[2]
+cos_inv = 0
+cos_rej = 0
+for i in range(10):
+    cos_inv += rates.sample_ctq_phi(mi,mf)[0]
+    cos_rej += rates.sample_B_ctq_phiq(ni,li,mi,nf,lf,mf)[0]
+cos_inv /= 10
+cos_rej /= 10
+print(cos_inv, cos_rej)
 '''
+
+
+
